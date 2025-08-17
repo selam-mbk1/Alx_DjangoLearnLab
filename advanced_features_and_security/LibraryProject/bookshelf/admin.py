002a6a9b8fa92aa1420358257db7b557546b545e
selam-mbk1 <selamawitbasaznew@gmail.com>
2025-08-17 06:12:56 +0300

    blog app is started

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,15 +1,25 @@
 from django.contrib import admin
+from .models import Book
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Show these columns in admin list
+    list_filter = ('author', 'publication_year')  # Add filter sidebar
+    search_fields = ('title', 'author')  # Add search functionality
+
+
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
         (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
